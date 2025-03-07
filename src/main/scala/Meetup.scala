fbe8946dca5e251c866b5a869dfe899e75ea1004
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-07 15:18:57 +0100

    solution

--- src/main/scala/Meetup.scala
+++ src/main/scala/Meetup.scala
@@ -5,14 +5,14 @@ case class Meetup(month: Int, year: Int):
 
    def day(dayOfWeek: Int, schedule: Schedule): LocalDate =
       val monthSeq = createMonthSeq(year, month).sortBy(_._1)
-      val sa = monthSeq.sliding(7, 7).toArray
+      val daysByWeek = monthSeq.groupBy { case (_, day) => day }
       val dayOfMonth = schedule match
-         case Schedule.Teenth => monthSeq.filter(p => p._1 > 12 & p._1 < 20).filter(p => p._2 == dayOfWeek).head._1
-         case Schedule.First  => sa(0).filter(p => p._2 == dayOfWeek).head._1
-         case Schedule.Second => sa(1).filter(p => p._2 == dayOfWeek).head._1
-         case Schedule.Third  => sa(2).filter(p => p._2 == dayOfWeek).head._1
-         case Schedule.Fourth => sa(3).filter(p => p._2 == dayOfWeek).head._1
-         case Schedule.Last   => monthSeq.filter(p => p._2 == dayOfWeek).last._1
+         case Schedule.Teenth => monthSeq.filter(t => t._1 > 12 & t._1 < 20 & t._2 == dayOfWeek).head._1
+         case Schedule.First  => daysByWeek(dayOfWeek).head._1
+         case Schedule.Second => daysByWeek(dayOfWeek)(1)._1
+         case Schedule.Third  => daysByWeek(dayOfWeek)(2)._1
+         case Schedule.Fourth => daysByWeek(dayOfWeek)(3)._1
+         case Schedule.Last   => daysByWeek(dayOfWeek).last._1
       LocalDate.of(year, month, dayOfMonth)
 
    private def createMonthSeq(year: Int, month: Int): Seq[(Int, Int)] =
